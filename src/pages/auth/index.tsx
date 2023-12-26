9f879569ae5e6967ed0285e9630b97aecda39a7a
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-27 00:00:12 +0530

    update src/pages/auth/index.tsx

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,8 +1,15 @@
+import Navbar from "@/components/Navbar/Navbar";
 import React from "react";
 
 type AuthPageProps = {};
 
-const index: React.FC<AuthPageProps> = () => {
-  return <div>Authentication Page</div>;
+const AuthPage: React.FC<AuthPageProps> = () => {
+  return (
+    <div className="bg-gradient-to-b from-black to-black h-screen relative">
+      <div className="max-w-7xl mx-auto ">
+        <Navbar />
+      </div>
+    </div>
+  );
 };
-export default index;
+export default AuthPage;
