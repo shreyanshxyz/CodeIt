114b23032ce0b8eb8744934892137e7e9ad274aa
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-27 00:08:04 +0530

    Added Sign In Button Styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,20 @@
+import Link from "next/link";
 import React from "react";
 
 type NavbarProps = {};
 
 const Navbar: React.FC<NavbarProps> = () => {
-  return <div className="font-bold text-white">Navbar</div>;
+  return (
+    <div className="flex items-center justify-between sm:px-12 px-2 md:px-24">
+      <Link href="/" className="flex items-center justify-center h-20">
+        <p className="text-white">Logo</p>
+      </Link>
+      <div className="flex items-center">
+        <button className="bg-green-600 text-white px-2 py-1 sm:px-4 rounded-md text-sm font-medium border-2 border-transparent hover:text-green-600 hover:bg-slate-50 hover:border-2 hover:border-green-600 transition duration-500 ease-in-out">
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
 };
 export default Navbar;
