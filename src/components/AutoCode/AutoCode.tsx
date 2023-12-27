50932b6e707208ad44c4aff75770c16bab51de00
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-27 10:50:57 +0530

    feat: create AutoCode.tsx

---
+++ src/components/AutoCode/AutoCode.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+type AutoCodeProps = {};
+
+const AutoCode: React.FC<AutoCodeProps> = () => {
+  return (
+    <div className="flex items-center justify-between h-[calc(100vh-5rem)] pointer-events-none select-none sm:px-12 px-2 md:px-24">
+      <p className="text-white">
+        Automatic Code Automatic Code Automatic Code Automatic Code Automatic
+        Code Automatic Code Automatic Code Automatic Code Automatic Code
+        Automatic Code Automatic Code Automatic Code Automatic Code Automatic
+        Code Automatic Code Automatic Code Automatic Code Automatic Code
+        Automatic Code Automatic Code Automatic Code Automatic Code Automatic
+        Code Automatic Code Automatic Code{" "}
+      </p>
+      <p className="text-white">
+        Automatic Code Automatic Code Automatic Code Automatic Code Automatic
+        Code Automatic Code Automatic Code Automatic Code Automatic Code
+        Automatic Code Automatic Code Automatic Code Automatic Code Automatic
+        Code Automatic Code Automatic Code Automatic Code Automatic Code
+        Automatic Code Automatic Code Automatic Code Automatic Code Automatic
+        Code Automatic Code Automatic Code{" "}
+      </p>
+    </div>
+  );
+};
+export default AutoCode;
