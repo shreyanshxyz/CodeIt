e78af0653f55fdde894488413d6d8e569d4fd0ab
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-28 00:31:39 +0530

    Added Support for React-Toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import "@/styles/globals.css";
 import Head from "next/head";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -12,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      <ToastContainer />
       <Component {...pageProps} />
     </RecoilRoot>
   );
