9352373cb40c8150172eca8003d76a82960dd131
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-27 11:22:01 +0530

    update Login.tsx

--- src/components/Modals/Login.tsx
+++ src/components/Modals/Login.tsx
@@ -5,9 +5,7 @@ const loading = false;
 const Login: React.FC<LoginProps> = () => {
   return (
     <form className="space-y-6 px-6 pb-4">
-      <h3 className="text-2xl font-semibold text-black items-center">
-        Sign in to CodeIt
-      </h3>
+      <h3 className="text-2xl font-semibold text-black">Sign in to CodeIt</h3>
       <div>
         <label
           htmlFor="email"
