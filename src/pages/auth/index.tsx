0fb97b6cae9ffcc7ce69c8715cf16074c517c7fe
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-27 17:35:56 +0530

    update src/pages/auth/index.tsx

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -2,14 +2,27 @@ import { authModalState } from "@/atoms/authModalAtom";
 import AutoCode from "@/components/AutoCode/AutoCode";
 import AuthModal from "@/components/Modals/AuthModal";
 import Navbar from "@/components/Navbar/Navbar";
-import React from "react";
+import { auth } from "@/firebase/firebase";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useRecoilValue } from "recoil";
 
 type AuthPageProps = {};
 
 const AuthPage: React.FC<AuthPageProps> = () => {
-  // Accessing out auth states for the modal
+  // Accessing our auth states for the modal
   const authModal = useRecoilValue(authModalState);
+  const [user, loading, error] = useAuthState(auth);
+  const [pageLoading, setPageLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) router.push("/");
+    if (!loading && !user) setPageLoading(false);
+  }, [user, router, loading]);
+
+  if (pageLoading) return null;
   return (
     <div className="bg-gradient-to-b from-black to-black h-screen relative">
       <div className="max-w-7xl mx-auto ">
