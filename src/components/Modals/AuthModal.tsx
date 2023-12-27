eaacb05c200d4ffdaa7d8e22b8abfb33c8683198
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-27 10:51:53 +0530

    feat: create AuthModal.tsx

---
+++ src/components/Modals/AuthModal.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type AuthModalProps = {};
+
+const AuthModal: React.FC<AuthModalProps> = () => {
+  return <div>Have a good coding</div>;
+};
+export default AuthModal;
