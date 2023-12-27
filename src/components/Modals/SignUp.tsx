d0a80cb9cb0d2c18f3200fb0172ba590f266680c
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-27 11:28:18 +0530

    update SignUp.tsx

--- src/components/Modals/SignUp.tsx
+++ src/components/Modals/SignUp.tsx
@@ -63,8 +63,7 @@ const SignUp: React.FC<SignUpProps> = () => {
 
       <button
         type="submit"
-        className="w-full text-white focus:ring-green-300 font-medium rounded-lg
-                text-sm px-5 py-2.5 text-center  bg-green-600 hover:bg-green-800
+        className="w-full text-white focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  bg-green-600 hover:bg-green-800
         "
       >
         {loading ? "Signing Up..." : "Sign Up"}
