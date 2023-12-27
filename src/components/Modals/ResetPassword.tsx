2a69338400a38c8b4e120ba6bcd1bc59f08ae9dd
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-12-27 11:28:12 +0530

    update ResetPassword.tsx

--- src/components/Modals/ResetPassword.tsx
+++ src/components/Modals/ResetPassword.tsx
@@ -3,6 +3,36 @@ import React from "react";
 type ResetPasswordProps = {};
 
 const ResetPassword: React.FC<ResetPasswordProps> = () => {
-  return <div>Have a good coding</div>;
+  return (
+    <form className="space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8">
+      <h3 className="text-2xl font-semibold text-black">Reset Password</h3>
+      <p className="text-sm text-black ">
+        Forgotten your password? Enter your e-mail address below, and we&apos;ll
+        send you an e-mail allowing you to reset it.
+      </p>
+      <div>
+        <label
+          htmlFor="email"
+          className="text-sm font-medium block mb-2 text-gray-900"
+        >
+          Your email
+        </label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="border-2 outline-none sm:text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 bg-gray-200 border-gray-500 placeholder-gray-400 text-gray-900"
+          placeholder="name@company.com"
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="w-full text-white  focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-green-600 hover:bg-green-800"
+      >
+        Reset Password
+      </button>
+    </form>
+  );
 };
 export default ResetPassword;
