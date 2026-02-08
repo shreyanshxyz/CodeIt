8f3bd2a4b00a3739158f3b971eb519c57d8341d5
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2026-02-08 14:05:18 +0530

    Refactor timer with react icons

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { FiRefreshCcw } from "react-icons/fi";
+import { FiRefreshCcw, FiClock } from "react-icons/fi";
 
 type TimerProps = {};
 
@@ -32,9 +32,12 @@ const Timer: React.FC<TimerProps> = () => {
   return (
     <div>
       {showTimer ? (
-        <div className="flex items-center space-x-2 bg-dark-fill-3 p-1.5 cursor-pointer rounded hover:bg-dark-fill-2">
-          <div>{formatTime(time)}</div>
+        <div className="flex items-center gap-2 bg-black-surface px-3 py-1.5 cursor-pointer rounded border border-border-subtle hover:bg-black-hover transition-all duration-150">
+          <span className="text-text-primary font-mono text-sm">
+            {formatTime(time)}
+          </span>
           <FiRefreshCcw
+            className="text-text-tertiary hover:text-text-primary transition-colors duration-150"
             onClick={() => {
               setShowTimer(false);
               setTime(0);
@@ -42,25 +45,13 @@ const Timer: React.FC<TimerProps> = () => {
           />
         </div>
       ) : (
-        <div
-          className="flex items-center p-1 h-8 hover:bg-dark-fill-3 rounded cursor-pointer"
+        <button
+          className="flex items-center justify-center p-1.5 h-8 w-8 hover:bg-black-surface rounded cursor-pointer transition-all duration-150"
           onClick={() => setShowTimer(true)}
+          aria-label="Start timer"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            width="1em"
-            height="1em"
-            fill="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              fillRule="evenodd"
-              d="M12 4a9 9 0 110 18 9 9 0 010-18zm0 2a7 7 0 100 14 7 7 0 000-14zm0 1.634a1 1 0 01.993.883l.007.117-.001 3.774 2.111 1.162a1 1 0 01.445 1.253l-.05.105a1 1 0 01-1.254.445l-.105-.05-2.628-1.447a1 1 0 01-.51-.756L11 13V8.634a1 1 0 011-1zM16.235 2.4a1 1 0 011.296-.269l.105.07 4 3 .095.08a1 1 0 01-1.19 1.588l-.105-.069-4-3-.096-.081a1 1 0 01-.105-1.319zM7.8 2.4a1 1 0 01-.104 1.319L7.6 3.8l-4 3a1 1 0 01-1.296-1.518L2.4 5.2l4-3a1 1 0 011.4.2z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </div>
+          <FiClock className="h-5 w-5 text-text-tertiary hover:text-text-primary" />
+        </button>
       )}
     </div>
   );
